2a289a25987be7423e3d1abb07c8040ea5173c15
Tarek Bchini <tarek.bchini83@gmail.com>
2023-06-10 13:41:41 +0200

    authentification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -18,12 +24,17 @@ const App = () => {
         <Routes>
           <Route path='/' element={<HomeScreen />} exact  />
           <Route path='/product/:id' element={<ProductScreen />}  />
+          <Route path='/cart' element={<CartScreen />}  />
+          <Route path='/Login' element={<LoginScreen />}  />
         </Routes>
       </Container> 
       
     </main>
 
     <Footer />
+
+    <ToastContainer/>
+
     </Router> 
     </>
   );
